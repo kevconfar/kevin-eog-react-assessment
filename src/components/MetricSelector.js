9310a020b7aec5e1e82027e62e5c08b34a34b983
Kevin <you@example.com>
2022-08-05 13:04:48 -0700

    metric selector now triggers the storage of measurements into redux

--- src/components/MetricSelector.js
+++ src/components/MetricSelector.js
@@ -1,8 +1,14 @@
-import React from 'react';
+/* eslint-disable react/jsx-props-no-spreading */
+import React, { useEffect } from 'react';
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
 import { useDispatch } from 'react-redux';
-import { setChosenMetrics } from '../Features/measurementsSlice';
+
+import { gql, useLazyQuery } from '@apollo/client';
+import { Typography } from '@material-ui/core';
+import {
+  setChosenMetrics, setMetricMeasurements,
+} from '../Features/measurementsSlice';
 
 const metricsList = [
   'waterTemp',
@@ -17,11 +23,43 @@ const searchStyle = {
   width: '30%', marginLeft: 'auto', marginRight: '10px', marginTop: '10px',
 };
 
+const GET_MEASUREMENTS = gql`
+    query($metrics: [MeasurementQuery]){
+      getMultipleMeasurements(input: $metrics) {
+        metric
+        measurements {
+          at
+          value
+          unit
+        }
+      }
+    }
+  `;
+
 export default function MetricSelector() {
   const dispatch = useDispatch();
+
+  const [getMeasurements, { loading, error, data }] = useLazyQuery(GET_MEASUREMENTS);
+
+  if (error) return <Typography color="error">{error}</Typography>;
+
   const handleChange = (metric) => {
     dispatch(setChosenMetrics({ metric }));
+    const before = new Date().valueOf();
+    const after = before - 1800000;
+    const metrics = metric.map((metricName) => ({ metricName, before, after }));
+    getMeasurements({ variables: { metrics } });
   };
+
+  useEffect(() => {
+    if (data) {
+      const { getMultipleMeasurements } = data;
+
+      if (getMultipleMeasurements !== undefined) {
+        dispatch(setMetricMeasurements({ getMultipleMeasurements }));
+      }
+    }
+  }, [loading]);
 
   return (
     <Autocomplete
@@ -33,9 +71,7 @@ export default function MetricSelector() {
       onChange={(e, value) => handleChange(value)}
       renderInput={(params) => (
         <TextField
-          /* eslint-disable */
           {...params}
-          /* eslint-enable */
           variant="standard"
           placeholder='Select...'
         />
